383f9118c53e038158bf36014fd696125ee440fa
luc siat <lucsiat@gmail.com>
2022-04-26 21:30:05 +0200

    added validation

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,6 @@ class List < ApplicationRecord
   has_one_attached :photo
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
-  validates :name, presence: true
+  validates :name, :list, presence: true
   validates_uniqueness_of :name
 end
